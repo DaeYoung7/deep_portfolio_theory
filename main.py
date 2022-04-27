c527eeb358747793eb0698bc59b15d16cf1e7a89
DaeYoung7 <44760826+DaeYoung7@users.noreply.github.com>
2022-04-27 14:36:23 +0900

    ready to run

--- main.py
+++ main.py
@@ -13,17 +13,18 @@ class MyModel(nn.Module):
         self.layer = layer
         self.input_shape = num_feature
         self.encoded_size = encoded_size
-        self.hidden_size = 256
+        self.hidden_size = 128
         self.output_shape = num_feature
 
         self.encoder_hl1 = nn.Linear(self.input_shape, self.hidden_size)
+        self.encoder_norm = nn.BatchNorm1d(self.hidden_size)
         self.encoder_hl2 = nn.Linear(self.hidden_size, self.encoded_size)
         self.encoder_lstm = nn.LSTM(self.input_shape, self.encoded_size, 1)
         self.decoder = nn.Linear(self.encoded_size, self.output_shape)
     def forward(self, inputs):
         if self.layer == 'linear':
             x = torch.relu(self.encoder_hl1(inputs))
-            x = torch.relu(self.encoder_hl2(x))
+            x = self.encoder_hl2(self.encoder_norm(x))
         else:
             x, _ = self.encoder_lstm(inputs)
             x = torch.squeeze(x[:,-1,:], dim=1)
@@ -52,14 +53,15 @@ else:
     test_label = torch.tensor(data[split:].values, dtype=torch.float32)
 
 num_feature = len(data.columns)
-encoded_size = num_feature // 3 + 2
+encoded_size = num_feature // 4
+encoded_size += (4 - encoded_size % 4)
 print(f'encoded_size : {encoded_size}')
 net = MyModel(layer_type, num_feature, encoded_size)
 optimizer = Adam(net.parameters(), lr=0.01, weight_decay=1e-5)
-scheduler = lr_scheduler.StepLR(optimizer, step_size=2500, gamma=0.1)
+scheduler = lr_scheduler.StepLR(optimizer, step_size=5000, gamma=0.1)
 loss_fn = nn.MSELoss()
 
-epochs = 10000
+epochs = 15000
 tlosses = []
 vlosses = []
 for epoch in range(epochs):
@@ -96,6 +98,6 @@ for i in range(len(data.columns)):
     plt.legend()
     plt.show()
 
-output_df = pd.DataFrame(output)
+output_df = pd.DataFrame(encoded_data)
 output_df.index = data.index
 output_df.to_csv('K200_deep_factor.csv')
